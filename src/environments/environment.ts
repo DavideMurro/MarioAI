7dab7a7a4c8b5e6c9014ea5ea230149f54866b8d
Davide Murro <e-demu@gft.com>
2023-12-04 17:51:01 +0100

    changing api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   appTitle: 'MarioAI',
   defaultLanguageCode: 'en',
   production: true,
-  stableDiffusionApiUrl: 'https://efacfb3cd40027eb14.gradio.live/sdapi/v1',
+  stableDiffusionApiUrl: 'https://d40c4bff0b6d670922.gradio.live/sdapi/v1',
 };
